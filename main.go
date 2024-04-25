c5d0b7c3f31fbb11fcad71eee04cc17c2ecd3447
2° Ten Pablo <guimaraes.pablo@CDS.LOCAL>
2024-04-25 14:46:59 -0300

    Subindo novos arquivos

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/pablomaiden/api-rest-golang-alura/database"
 	"github.com/pablomaiden/api-rest-golang-alura/models"
 	"github.com/pablomaiden/api-rest-golang-alura/routes"
 )
@@ -13,6 +14,8 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleResquest()
--- routes/routes.go
+++ routes/routes.go
@@ -4,14 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/pablomaiden/api-rest-golang-alura/controllers"
+	"github.com/pablomaiden/api-rest-golang-alura/middleware"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
